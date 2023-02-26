9c0c73c8eae8b4f4a9e7686dc06512bd54e50c05
Eric Yarham <eric.yarham@gmail.com>
2023-02-26 09:01:27 -0500

    adding buttons to customer dashboard

--- src/demo/customer/CustomerTab.jsx
+++ src/demo/customer/CustomerTab.jsx
@@ -1,11 +1,17 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 const CustomerTab = () => {
   return (
     <div>
       <h3>Customer Dashboard</h3>
-      <div>Ready for pickup button</div>
-      <div>View history button</div>
+      <p>One Bucket</p>
+      <div>
+        <Button variant="contained">Ready For Pickup</Button>
+      </div>
+      <div>
+        <Button>View History</Button>
+      </div>
     </div>
   )
 }
